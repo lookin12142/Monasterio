52083e42357fb0051434f295cfcc3d82b3553df8
lucas ponce <apolion2009tio@gmail.com>
2024-10-18 01:12:25 -0500

    weba

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,10 +11,12 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-connectDb();
 createAdmin();
 
 routesLoader(app);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+connectDb().then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  });
+  
