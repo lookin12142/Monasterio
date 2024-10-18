fbf2d4604d403da222b396c6ca5841fbcbeedd25
lucas ponce <apolion2009tio@gmail.com>
2024-10-18 15:28:27 -0500

    2.1

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,14 +9,14 @@ import createAdmin from './config/createAdmin.js';
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-createAdmin();
-
-routesLoader(app);
 
 const PORT = process.env.PORT || 3000;
+
 connectDb().then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  });
-  
+  createAdmin();
+  routesLoader(app);
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
--- backend/src/models/usermodel.js
+++ backend/src/models/usermodel.js
@@ -1,7 +1,7 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../config/database.js';
 
-const User = sequelize.define('user', {
+const User = sequelize.define('User', {
   name: {
     type: DataTypes.STRING,
     allowNull: false
