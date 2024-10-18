d2505bcf148ec6bfcfdef761a026f8e1ff2febd9
lucas ponce <apolion2009tio@gmail.com>
2024-10-18 11:15:43 -0500

    2

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,32 +1,28 @@
 import jwt from 'jsonwebtoken';
 
 const auth = (req, res, next) => {
-  const authHeader = req.header('Authorization');
-  console.log('Token recibido:', authHeader);
-
-  if (!authHeader) {
-    console.log('No se recibió token');
-    return res.status(401).json({ message: 'No token, authorization denied' });
-  }
-
-  const token = authHeader.split(' ')[1];
-  console.log('Token separado:', token);
-
-  try {
-    req.user = decoded.user;
-    console.log('Usuario:', req.user);
-
-    if (!req.user.isadmin) {
-      console.log('Usuario no es admin');
-      return res.status(403).json({ message: 'Access denied, not an admin' });
+    const authHeader = req.header('Authorization');
+    console.log('Token recibido:', authHeader);
+    if (!authHeader) {
+        console.log('No se recibió token');
+        return res.status(401).json({ message: 'No token, authorization denied' });
     }
-
-    console.log('Autenticación exitosa');
-    next();
-  } catch (err) {
-    console.error('Error al verificar el token:', err);
-    res.status(401).json({ message: 'Token is not valid' });
-  }
+    const token = authHeader.split(' ')[1];
+    console.log('Token separado:', token);
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        req.user = decoded.user;
+        console.log('Usuario:', req.user);
+        if (!req.user.isadmin) {
+            console.log('Usuario no es admin');
+            return res.status(403).json({ message: 'Access denied, not an admin' });
+        }
+        console.log('Autenticación exitosa');
+        next();
+    } catch (err) {
+        console.error('Error al verificar el token:', err);
+        res.status(401).json({ message: 'Token is not valid' });
+    }
 };
 
 export default auth;
